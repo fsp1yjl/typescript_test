bd8d13abfaf980eef49ba9690ab19758f94ffed2
unknown <eric.feng@bh-sh-eric-feng-p.both.local>
2016-09-23 13:36:43 +0800

    create tag v0.1

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@
 const gulp = require('gulp')
 const typescript = require('gulp-typescript')
 const rimraf = require('rimraf')
+const clean = require('gulp-clean')
 //const rename = require('gulp-rename')
 
 
-gulp.task('compoler:ts', function() {
+gulp.task('compoler:ts',['clean'], function() {
     return gulp.src("./src/**/*.ts")
         .pipe(typescript( typescript.createProject("./src/tsconfig.json") ))
         .pipe(gulp.dest('build/serve'))
@@ -15,6 +16,11 @@ gulp.task('compoler:ts', function() {
 gulp.task('build:html',function() {
     return gulp.src("./src/test_page/**/*.html")
         .pipe(gulp.dest('build/serve/test_page'))
+})
+
+gulp.task('clean',function(){
+    return gulp.src("./build",{read:false})
+        .pipe(clean())
 })
 
 gulp.task('default',['compoler:ts','build:html']);
